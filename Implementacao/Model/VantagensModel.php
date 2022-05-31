69aefec4a204528ca4639d6b8a8f111f1f85ada6
Rodolfo Rocha Rodrigues <71857852+rodolfo12381@users.noreply.github.com>
2022-05-31 20:49:50 -0300

    Add files via upload

--- Implementacao/Model/VantagensModel.php
+++ Implementacao/Model/VantagensModel.php
@@ -4,7 +4,9 @@ class VantagensModel {
 
     public static function update ($con,$data)
     {
-        $sql = "UPDATE tbvantagem SET ($data= $data) WHERE  condition = $data";
+    
+        $sql = "UPDATE tbvantagem SET nome = '{$data['nome']}', descricao = '{$data['descricao']}', custo = '{$data['custo']}',
+        foto = '{$data['foto']}', fk_empresa = '{$data['id']}' WHERE  id = {$data['id_vantagem']}";
         $result = mysqli_query($con,$sql);
         if(!$result){
             echo 'Erro ao atualizar Vantagem';
@@ -15,10 +17,8 @@ class VantagensModel {
     public static function insert ($con,$data)
     {
         $sql = "INSERT INTO tbvantagem (nome,descricao,custo,foto,fk_empresa)
-        VALUES ('{$data['nome']}','{$data['descricao']}','{$data['custo']}','{$data['foto']}'),{$data['id']});";
+        VALUES ('{$data['nome']}','{$data['descricao']}','{$data['custo']}','{$data['foto']}',{$data['id']});";
         $result = mysqli_query($con,$sql);
-        print_r($sql);
-        die();
         if(!$result) {
             echo 'Erro ao cadastrar Vantagem';
             die();
@@ -27,7 +27,7 @@ class VantagensModel {
 
     public static function delete ($con,$data)
     {
-        $sql = "DELETE FROM tbvantagem WHERE condition = $data";
+        $sql = "DELETE FROM tbvantagem WHERE id = $data";
             $result = mysqli_query($con,$sql);
             if(!$result){
                 echo 'Erro ao deletar Vantagem';
@@ -43,6 +43,16 @@ class VantagensModel {
             echo 'Erro ao buscar Vantagem';
             die();
         }
+    }
+    public static function selectById ($con,$data)
+    {
+        $sql = "SELECT * FROM tbvantagem WHERE fk_empresa = $data";
+        $result = mysqli_query($con,$sql);
+        $arrayVantagens= array();
+        while ($row = $result->fetch_assoc()){
+            $arrayVantagens [] = $row;
+        }
+        return $arrayVantagens;
     }
 }
 
